4e61b0eed14c394fcf12fe975541ce2c6bb2b99d
Diegocj01 <di.cortesj@duocuc.cl>
2022-05-09 22:22:36 -0400

    Se hicieron views y urls

--- CesfamApp/urls.py
+++ CesfamApp/urls.py
@@ -4,4 +4,12 @@ from .views import *
 
 urlpatterns = [
   path('',home,name="home"),
+  path('hospitales',hospitales,name="hospitales"),
+  path('contacto',contacto,name="contacto"),
+  path('equipo',equipo,name="equipo"),
+  path('mision',mision,name="mision"),
+  path('valores',valores,name="valores"),
+  path('vision',vision,name="vision"),
+  path('login',login,name="login"),
+  path('registro',registro,name="registro"),
 ]
--- CesfamApp/views.py
+++ CesfamApp/views.py
@@ -3,4 +3,29 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'CesfamWeb/index.html')
+
+def hospitales(request):
+    return render(request, 'CesfamWeb/hospitales.html')
+
+def contacto(request):
+    return render(request, 'CesfamWeb/contacto.html')
+
+
+def equipo(request):
+    return render(request, 'CesfamWeb/conozcanos/equipo.html')
+
+def mision(request):
+    return render(request, 'CesfamWeb/conozcanos/mision.html')
+
+def valores(request):
+    return render(request, 'CesfamWeb/conozcanos/valores.html')
+
+def vision(request):
+    return render(request, 'CesfamWeb/conozcanos/vision.html')
+
+def login(request):
+    return render(request, 'RegistroLogin/login.html')
+
+def registro(request):
+    return render(request, 'RegistroLogin/registro.html')
