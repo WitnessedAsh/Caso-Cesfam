d87267407eb44fccfc405a60d9cdc98812deaa06
vgamboacacciuttolo <WitnessedAsh@gmail.com>
2022-05-16 16:23:34 -0400

    list medicamentos

--- CesfamApp/urls.py
+++ CesfamApp/urls.py
@@ -5,12 +5,10 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
   path('',home,name="home"),
-  path('equipo',equipo,name="equipo"),
-  path('mision',mision,name="mision"),
-  path('valores',valores,name="valores"),
-  path('vision',vision,name="vision"),
+  #Medicamentos
+  path('listmedicamentos',listmedicamentos,name="listmedicamentos"),
   path('reserva',reserva,name="reserva"),
-  #path('login',login,name="login"),
+  #Loguear y Desloguear
   path('login/', LoginView.as_view(template_name='RegistroLogin/login.html'),name="login"),
   path('logout/', LogoutView.as_view(template_name='RegistroLogin/logout.html'),name="logout"),
 ]
--- CesfamApp/views.py
+++ CesfamApp/views.py
@@ -5,20 +5,10 @@ from django.shortcuts import render, redirect
 def home(request):
     return render(request, 'CesfamWeb/index.html')
 
-def equipo(request):
-    return render(request, 'CesfamWeb/conozcanos/equipo.html')
-
-def mision(request):
-    return render(request, 'CesfamWeb/conozcanos/mision.html')
-
-def valores(request):
-    return render(request, 'CesfamWeb/conozcanos/valores.html')
-
-def vision(request):
-    return render(request, 'CesfamWeb/conozcanos/vision.html')
+def listmedicamentos(request):
+    return render(request, 'CesfamWeb/listmedicamentos.html')
 
 def reserva(request):
     return render(request, 'CesfamWeb/reserva.html')    
 
-#def login(request):
-#    return render(request, 'RegistroLogin/login.html')
+
