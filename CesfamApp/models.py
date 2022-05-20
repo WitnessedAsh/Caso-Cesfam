806129a7cf0925109b0e85db8ccf0963ca963352
nicosegoviaatalah <104632808+nicosegoviaatalah@users.noreply.github.com>
2022-05-20 09:29:42 -0700

    Update models.py

--- CesfamApp/models.py
+++ CesfamApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Production(models.Model):
+  nombre = models.Charfield(max_length=64)
+  categoria = models.Charfield(max_length=32
+  precio = models.IntegerField()
+                               
+                               
+     def __str__(self):
+           return f'{self.nombre} ->{self.precio}'                    
+                               
+    
